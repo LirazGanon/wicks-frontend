e13d3400cbb53ce2994279acd7fdfeabcc3b9625
לירז גנון <lirazganon@gmail.com>
2022-11-30 18:23:54 +0200

    starting container cmp

---
+++ src/cmps/waps/wap-card.vue
@@ -0,0 +1,25 @@
+<template>
+    <section class="wap-card">
+        <pre>{{cmp}}</pre>
+        <h1>{{cmp.heading.txt}}</h1>
+        <h4>card - {{ cmp.id }}</h4>
+    </section>
+
+</template>
+<script>
+export default {
+    name: 'dynamic-review-cmp',
+    props: { cmp: Object },
+    components: {},
+    data() {
+        return {};
+    },
+    created() { },
+    methods: {},
+    computed: {},
+    unmounted() { },
+};
+</script>
+<style>
+
+</style>

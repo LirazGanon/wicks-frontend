495d621a6dd86a433644f44e73f5ca1ae46a172c
Nadav Shambi <113799531+NadavShambi@users.noreply.github.com>
2022-12-01 13:49:22 +0200

    progress

--- src/cmps/editor-top.vue
+++ src/cmps/editor-top.vue
@@ -1,13 +1,45 @@
 <template>
 
-    <header>
-
+    <section class="editor-header">
         <router-link to="/">
             <span role="img" aria-label="logo">
                 <img class="logo" src="@/assets/wicks-header-logo.png" alt="">
             </span>
         </router-link>
-    </header>
+    </section>
+
+    <section class="editor-top main-container">
+        <section class="flex space-between">
+            <section class="view-port-editor flex">
+                <div class="material-symbols-outlined icon">
+                    desktop_windows
+                </div>
+                <div class="material-symbols-outlined icon">
+                    tablet_mac
+                </div>
+                <div class="material-symbols-outlined icon">
+                    phone_iphone
+                </div>
+
+                <div class="dynamic-url">
+                    <div class="material-symbols-outlined">
+                        vpn_key
+                    </div>
+                    <span>https</span>
+                    <span>://wicks.renderapp.com/</span>
+                    <span contenteditable="true">my-site</span>
+                </div>
+
+            </section>
+
+            <div class="site-preview">
+                <span>Preview</span>
+            </div>
+
+
+        </section>
+
+    </section>
 
 
 </template>

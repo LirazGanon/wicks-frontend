e18b8d8477016d5eead868c01b1c9db0ae5bb438
לירז גנון <lirazganon@gmail.com>
2022-12-02 11:41:16 +0200

    design

--- src/cmps/waps/wap-gallery.vue
+++ src/cmps/waps/wap-gallery.vue
@@ -1,13 +1,10 @@
 <template>
     <section class="wap-gallery" :class="cmp.classes">
-        <img class="animate__animated animate__slideInRight" v-for="img in cmp.info.imgs" :src="img.src">
+        <img v-for="img in cmp.info.imgs" :src="img.src">
     </section>
 
 </template>
 <script>
-
-import 'animate.css'
-
 export default {
     name: 'dynamic-gallery-cmp',
     props: { cmp: Object },
--- src/main.js
+++ src/main.js
@@ -3,24 +3,25 @@ import { createApp } from 'vue'
 import { router } from './router.js'
 import { store } from './store'
 
+
 import {
-    focusDirective,
+  focusDirective,
     rainbowDirective,
     customOnDirective,
     clickOutsideDirective,
   } from '../directives'
 
-
-import './assets/styles/main.scss'
-import rootCmp from './root-cmp.vue'
-
-const app = createApp(rootCmp)
-app.use(router)
-app.use(store)
-
-app.directive('focus', focusDirective)
-app.directive('rainbow', rainbowDirective)
-app.directive('custom-on', customOnDirective)
-app.directive('click-outside', clickOutsideDirective)
+  
+  import './assets/styles/main.scss'
+  import rootCmp from './root-cmp.vue'
+  
+  const app = createApp(rootCmp)
+  app.use(router)
+  app.use(store)
+  
+  app.directive('focus', focusDirective)
+  app.directive('rainbow', rainbowDirective)
+  app.directive('custom-on', customOnDirective)
+  app.directive('click-outside', clickOutsideDirective)
 
 app.mount('#app')

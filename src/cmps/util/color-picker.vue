5f613e2bb523441104e617952c0fd2fab0e52f3f
לירז גנון <lirazganon@gmail.com>
2022-12-02 16:29:03 +0200

    design

---
+++ src/cmps/util/color-picker.vue
@@ -0,0 +1,32 @@
+<template>
+    <section class="color-picker">
+        <div class="color-picker-item" v-for="(item, index) in colors" :key="index">
+            <div :style="{backgroundColor:item}">{{ item }}</div>
+        </div>
+
+    </section>
+
+</template>
+<script>
+export default {
+    name: 'color-picker',
+    props: {
+     
+    },
+    components: {},
+    data() {
+        return {
+            colors: ['#6D67E4', '#937DC2', '#6ECCAF', '#F2DEBA', '#F79F79', '#F7D08A']
+
+        }
+    },
+    created() {
+    },
+    methods: {
+    },
+    computed: {}
+}
+</script>
+<style>
+
+</style>

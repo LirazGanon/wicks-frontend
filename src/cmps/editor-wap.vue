af01fc90e1d8210c3c6730a490ff1ce15903f5c0
לירז גנון <lirazganon@gmail.com>
2022-12-02 19:49:10 +0200

    fixed commit

--- src/cmps/editor-wap.vue
+++ src/cmps/editor-wap.vue
@@ -11,7 +11,7 @@
         <!-- 
           <pre>
             {{ item }}
-            XXXXXXXXXXXXXXXX
+            XXXXXXXXXXXXXXX
           </pre> -->
         <component :is="(item.type || 'wap-header')" :cmp="item" @log="log" @openEditor="$emit('openEditor', $event)" />
 

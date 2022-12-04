08204cce645487e90983cf53b972ce33b8a42b01
לירז גנון <lirazganon@gmail.com>
2022-12-04 09:17:05 +0200

    cloudnery

--- src/main.js
+++ src/main.js
@@ -4,7 +4,7 @@ import { router } from './router.js'
 import { store } from './store'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
-import Tooltip from '../tooltip'
+import Tooltip from '../lib/tooltip'
 
 
 

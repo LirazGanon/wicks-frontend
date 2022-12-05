aa5c597ee26b640d41d4fcf57df3ec7226aca3b2
Nadav Shambi <113799531+NadavShambi@users.noreply.github.com>
2022-12-05 19:07:02 +0200

    bug fix droppable id changes

--- src/cmps/util/slider.vue
+++ src/cmps/util/slider.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="slider-container">
         <label>
-            <span>{{ value }}</span>
+            <span></span>
             <input type="range" v-model="value" min="0" max="100" class="slider">
+            
         </label>
 
-
+        <h1>CSS Range Slider</h1>
+        <h3>JS used for color-change and % label</h3>
+        <input type="range"   v-model="value" min="0" max="100" >
+        <div id="h4-container">
+            <div id="h4-subcontainer">
+                <h4 :style="getLocation">{{ value }}<span></span></h4>
+            </div>
+        </div>
 
 
     </div>
@@ -14,6 +22,24 @@
 <script>
 
 import { ref } from 'vue'
+
+
+// $(function () {
+// 	var rangePercent = $('[type="range"]').val();
+// 	$('[type="range"]').on("change input", function () {
+// 		rangePercent = $('[type="range"]').val();
+// 		$("h4").html(rangePercent + "<span></span>");
+// 		$('[type="range"], h4>span').css(
+// 			"filter",
+// 			"hue-rotate(-" + rangePercent + "deg)"
+// 		);
+// 		// $('h4').css({'transform': 'translateX(calc(-50% - 20px)) scale(' + (1+(rangePercent/100)) + ')', 'left': rangePercent+'%'});
+// 		$("h4").css({
+// 			transform: "translateX(-50%) scale(" + (1 + rangePercent / 100) + ")",
+// 			left: rangePercent + "%"
+// 		});
+// 	});
+// });
 
 export default {
     name: 'color-picker',
@@ -32,7 +58,11 @@ export default {
     },
     methods: {
     },
-    computed: {},
+    computed: {
+        getLocation(){
+            return {transformX: this.value + '%'}
+        }
+    },
     watch: {
         change: function () {
             this.value = 50
@@ -43,9 +73,113 @@ export default {
 
 
 <style>
-.slider {
-    background: #555;
+
+
+
+html input[type=range] {
+    outline: 0;
+    border: 0;
+    border-radius: 500px;
+    width: 400px;
+    max-width: 100%;
+    margin: 24px 0 16px;
+    transition: box-shadow 0.2s ease-in-out;
 }
 
-/* input */
+@media screen and (-webkit-min-device-pixel-ratio: 0) {
+    html input[type=range] {
+        overflow: hidden;
+        height: 40px;
+        -webkit-appearance: none;
+        background-color: #ddd;
+    }
+
+    html input[type=range]::-webkit-slider-runnable-track {
+        height: 40px;
+        -webkit-appearance: none;
+        color: #444;
+        -webkit-transition: box-shadow 0.2s ease-in-out;
+        transition: box-shadow 0.2s ease-in-out;
+    }
+
+    html input[type=range]::-webkit-slider-thumb {
+        width: 40px;
+        -webkit-appearance: none;
+        height: 40px;
+        cursor: ew-resize;
+        background: #fff;
+        box-shadow: -340px 0 0 320px #1597ff, inset 0 0 0 40px #1597ff;
+        border-radius: 50%;
+        -webkit-transition: box-shadow 0.2s ease-in-out;
+        transition: box-shadow 0.2s ease-in-out;
+        position: relative;
+    }
+
+    html input[type=range]:active::-webkit-slider-thumb {
+        background: #fff;
+        box-shadow: -340px 0 0 320px #1597ff, inset 0 0 0 3px #1597ff;
+    }
+}
+
+html input[type=range]::-moz-range-progress {
+    background-color: #43e5f7;
+}
+
+html input[type=range]::-moz-range-track {
+    background-color: #9a905d;
+}
+
+html input[type=range]::-ms-fill-lower {
+    background-color: #43e5f7;
+}
+
+html input[type=range]::-ms-fill-upper {
+    background-color: #9a905d;
+}
+
+#h4-container {
+    width: 400px;
+    max-width: 100%;
+    padding: 0 20px;
+    box-sizing: border-box;
+    position: relative;
+}
+
+#h4-container #h4-subcontainer {
+    width: 100%;
+    position: relative;
+}
+
+#h4-container #h4-subcontainer h4 {
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    position: absolute;
+    top: 0;
+    width: 40px;
+    height: 40px;
+    color: #fff !important;
+    font-size: 12px;
+    transform-origin: center -10px;
+    transform: translateX(-50%);
+    transition: margin-top 0.15s ease-in-out, opacity 0.15s ease-in-out;
+}
+
+#h4-container #h4-subcontainer h4 span {
+    position: absolute;
+    width: 100%;
+    height: 100%;
+    top: 0;
+    left: 0;
+    background-color: #1597ff;
+    border-radius: 0 50% 50% 50%;
+    transform: rotate(45deg);
+    z-index: -1;
+}
+
+input:not(:active)+#h4-container h4 {
+    opacity: 0;
+    margin-top: -50px;
+    pointer-events: none;
+}
 </style>

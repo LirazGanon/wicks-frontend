11f5e00c4568ea1ee4b7d2673affb8227d4cdc66
לירז גנון <lirazganon@gmail.com>
2022-12-07 23:05:56 +0200

    Thanks Nadav for adding me the cmp

--- src/cmps/waps/wap-container.vue
+++ src/cmps/waps/wap-container.vue
@@ -1,7 +1,7 @@
 <template>
 
     <section class="main-layout full  wap-container-wrapper" :style="cmp.style" :class="cmp.classes">
-
+   
         <section class="wap-container" :class="cmp.classes">
             <h1 v-if="cmp.info.heading?.txt" :style="cmp.info.heading.style">
                 {{ cmp.info.heading.txt }}

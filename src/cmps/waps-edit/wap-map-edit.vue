9aa94143ad90b2f3bfe55bbaef663c136cb6fc80
Nadav Shambi <113799531+NadavShambi@users.noreply.github.com>
2022-12-09 12:16:24 +0200

    nav fixes

--- src/cmps/waps-edit/wap-map-edit.vue
+++ src/cmps/waps-edit/wap-map-edit.vue
@@ -2,8 +2,6 @@
 <!-- thisns needed to be edited:
 lat lng with input to change the markers(cmp.info.markers.position.lat/.lng)
 center with input
-on lines 97-98 the lat lng is given on ckick on the map
-there is some errors in this page aboute mutationing the store outside of the store and some more errors that need to be inspected
 -->
 
 <template>
@@ -16,12 +14,12 @@ there is some errors in this page aboute mutationing the store outside of the st
           <div id="siteNotice"></div>
 
           <h1 id="firstHeading" class="firstHeading" v-if="m.title" contenteditable="true"
-            @click.stop@mousedown.stop="openEditor('heading')">title
+            @click.stop @mousedown.stop="openEditor('heading')">title
           </h1>
 
           <div id="bodyContent">
 
-            <b contenteditable="true" @click.stop@mousedown.stop="openEditor('title')">sub title</b>
+            <b contenteditable="true" @click.stop @mousedown.stop="openEditor('title')">sub title</b>
 
             <p v-if="m.details" contenteditable="true" @mousedown.stop="openEditor('details')">
               here you can write things about your buissness or whatever it is that you want to share with the visitors
@@ -47,71 +45,68 @@ export default {
   },
 
   methods: {
-      openSectionEditor() {
-        const payload = {
-          el: { type: 'section' },
-          currCmp: this.cmp,
-          path: this.getPath()
-        }
-        this.$emit('openEditor', payload)
-      },
-      openEditor(key, idx) {
-        let el = (idx !== undefined) ? this.cmp.info[key][idx] : this.cmp.info[key]
-        el = utilService.copy(el)
-        const payload = {
-          key,
-          path: this.getPath(idx),
-          el,
-          currCmp: this.cmp,
-          elIdx: idx
-        }
-        this.$emit('openEditor', payload)
-      },
-      updateCmp(ev, key, idx) {
-        let wap = this.$store.getters.getWapToEdit
-        wap = utilService.copy(wap)
-        const path = this.getPath(idx)
-        let cmp = utilService.copy(this.cmp)
-        let el
-        if (idx !== undefined) {
-          el = cmp.info[key][idx]
-        } else {
-          el = cmp.info[key]
-        }
-        if (el.txt === ev.target.innerText) return
-        el.txt = ev.target.innerText
+    //   openSectionEditor() {
+    //     const payload = {
+    //       el: { type: 'section' },
+    //       currCmp: this.cmp,
+    //       path: this.getPath()
+    //     }
+    //     this.$emit('openEditor', payload)
+    //   },
+    //   openEditor(key, idx) {
+    //     let el = (idx !== undefined) ? this.cmp.info[key][idx] : this.cmp.info[key]
+    //     el = utilService.copy(el)
+    //     const payload = {
+    //       key,
+    //       path: this.getPath(idx),
+    //       el,
+    //       currCmp: this.cmp,
+    //       elIdx: idx
+    //     }
+    //     this.$emit('openEditor', payload)
+    //   },
+    //   updateCmp(ev, key, idx) {
+    //     let wap = this.$store.getters.getWapToEdit
+    //     wap = utilService.copy(wap)
+    //     const path = this.getPath(idx)
+    //     let cmp = utilService.copy(this.cmp)
+    //     let el
+    //     if (idx !== undefined) {
+    //       el = cmp.info[key][idx]
+    //     } else {
+    //       el = cmp.info[key]
+    //     }
+    //     if (el.txt === ev.target.innerText) return
+    //     el.txt = ev.target.innerText
 
-        wap.cmps[path.fatherIdx] = cmp
-        try {
-          this.$store.dispatch({ type: 'updateWapFull', wap })
-        } catch {
-          console.log('ops')
-        }
-      },
-      getPath(idx) {
-        const wap = this.$store.getters.getWapToEdit
-        const cmpIdx = wap.cmps.findIndex(cmp => cmp.id === this.cmp.id)
-        return { fatherIdx: cmpIdx, elIdx: idx, id: this.cmp.id }
-      },
+    //     wap.cmps[path.fatherIdx] = cmp
+    //     try {
+    //       this.$store.dispatch({ type: 'updateWapFull', wap })
+    //     } catch {
+    //       console.log('ops')
+    //     }
+    //   },
+    //   getPath(idx) {
+    //     const wap = this.$store.getters.getWapToEdit
+    //     const cmpIdx = wap.cmps.findIndex(cmp => cmp.id === this.cmp.id)
+    //     return { fatherIdx: cmpIdx, elIdx: idx, id: this.cmp.id }
+    //   },
 
-    mapClicked(ev) {
-console.log(ev.latLng.lat())
-console.log(ev.latLng.lng())
-// console.log(this. GoogleMap)
-      // GoogleMap.addListener("click", (mapsMouseEvent) => {
-      //   // Close the current InfoWindow.
-      //   infoWindow.close();
+    mapClicked() {
+      GoogleMap.addListener("click", (mapsMouseEvent) => {
+        // Close the current InfoWindow.
+        infoWindow.close();
 
-      //   // Create a new InfoWindow.
-      //   infoWindow = new google.maps.InfoWindow({
-      //     position: mapsMouseEvent.latLng,
-      //   });
-      //   console.log(mapsMouseEvent.latLng)
-      //   infoWindow.setContent(
-      //     JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
-      //   );
-      //   infoWindow.open(map);
-      // });
+        // Create a new InfoWindow.
+        infoWindow = new google.maps.InfoWindow({
+          position: mapsMouseEvent.latLng,
+        });
+        console.log(mapsMouseEvent.latLng)
+        infoWindow.setContent(
+          JSON.stringify(mapsMouseEvent.latLng.toJSON(), null, 2)
+        );
+        infoWindow.open(map);
+      });
     }
 
 },
